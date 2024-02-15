fe837f4f4762f6506081f92d225cf121fa9e5e93
Vzyree-619 <99886505+Vzyree-619@users.noreply.github.com>
2024-02-15 10:38:45 +0500

    update index.js
    manay isme image ka issue hal kia h

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ let mainBox = document.querySelector(".parent")
 
 
 function Animals (){
-      mainBox.style.backgroundImage = "url(/cheetah.jpg)"
+      mainBox.style.backgroundImage = "url(./cheetah.jpg)"
 }
 
 function horse (){
-    mainBox.style.backgroundImage = "url(/horse.jpg)"
+    mainBox.style.backgroundImage = "url(./horse.jpg)"
 }
 
 
 function car ( ){
-    mainBox.style.backgroundImage = "url(/car.jpg)"
+    mainBox.style.backgroundImage = "url(./car.jpg)"
 }
