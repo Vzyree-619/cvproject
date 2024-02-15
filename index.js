c9bdf84c21f65d00e43fa5976f626a02e2b669b5
Vzyree-619 <99886505+Vzyree-619@users.noreply.github.com>
2024-02-15 10:31:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let mainBox = document.querySelector(".parent")
+
+
+function Animals (){
+      mainBox.style.backgroundImage = "url(/cheetah.jpg)"
+}
+
+function horse (){
+    mainBox.style.backgroundImage = "url(/horse.jpg)"
+}
+
+
+function car ( ){
+    mainBox.style.backgroundImage = "url(/car.jpg)"
+}
